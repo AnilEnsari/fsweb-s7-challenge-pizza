a29e8badbd25724c82562ec95a6127b58d798a21
AnilEnsari <anilensari@gmail.com>
2023-08-14 22:05:42 +0300

    cssalmostdone

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="mainContainer">
       <h1>Teknolojik Yemekler</h1>
       <h1>Wellcome to Home Page</h1>
       <a href="order">
--- src/components/ZorunluFormlar.js
+++ src/components/ZorunluFormlar.js
@@ -4,31 +4,33 @@ const ZorunluFormlar = () => {
   return (
     <div className="zorunluFormMain ">
       <form>
-        <span className="size">
-          <h3 className="inline">Boyut Seç</h3>
-          <div>
-            <input type="radio" id="kucuk" name="boyut" value="kucuk"></input>
-            <label htmlFor="kucuk">Küçük</label>
-          </div>
-          <div>
-            <input type="radio" name="boyut" value="orta" id="orta"></input>
-            <label htmlFor="orta">Orta</label>
-          </div>
-          <div>
-            <input type="radio" name="boyut" value="buyuk" id="buyuk"></input>
-            <label htmlFor="buyuk">Büyük</label>
-          </div>
-        </span>
-        <span className="hamurKalinlik">
-          <h3> Hamur Seç</h3>
-          <label htmlFor="hamursec">
-            <select name="kalinliklar" id="kalinliklar">
-              <option value="">Hamur Kalınlığı</option>
-              <option value="ince">İnce</option>
-              <option value="kalin">Kalın</option>
-            </select>
-          </label>
-        </span>
+        <div className="flexyap">
+          <span className="size">
+            <h3 className="inline">Boyut Seç</h3>
+            <div>
+              <input type="radio" id="kucuk" name="boyut" value="kucuk"></input>
+              <label htmlFor="kucuk">Küçük</label>
+            </div>
+            <div>
+              <input type="radio" name="boyut" value="orta" id="orta"></input>
+              <label htmlFor="orta">Orta</label>
+            </div>
+            <div>
+              <input type="radio" name="boyut" value="buyuk" id="buyuk"></input>
+              <label htmlFor="buyuk">Büyük</label>
+            </div>
+          </span>
+          <span className="hamurKalinlik">
+            <h3> Hamur Seç</h3>
+            <label htmlFor="hamursec">
+              <select name="kalinliklar" id="kalinliklar">
+                <option value="">Hamur Kalınlığı</option>
+                <option value="ince">İnce</option>
+                <option value="kalin">Kalın</option>
+              </select>
+            </label>
+          </span>
+        </div>
       </form>
     </div>
   );
