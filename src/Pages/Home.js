9f7b3cb763d54ba3b0cd04937e6e4193b24ca3ac
AnilEnsari <anilensari@gmail.com>
2023-08-14 23:47:56 +0300

    imagedone

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,9 +3,8 @@ import "./home.css";
 
 const Home = () => {
   return (
-    <div className="mainContainer">
+    <div className="mainContainer1">
       <h1>Teknolojik Yemekler</h1>
-      <h1>Wellcome to Home Page</h1>
       <a href="order">
         <button>Acıktım</button>
       </a>
