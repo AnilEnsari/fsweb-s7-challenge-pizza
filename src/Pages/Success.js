50d1b21136456f35baf55af2b241eda1e45a0d47
AnilEnsari <anilensari@gmail.com>
2023-08-15 12:19:47 +0300

    mvppagesdone

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./success.css";
 
 const Success = () => {
   return (
-    <div>
-      <a href="/"> Ana Sayfa</a>
-      <h1>Tebrikler Sipariş Verdiniz</h1>
+    <div className="mainContainer">
+      <div className="basliklar">
+        <h2>Teknolojik Yemekler</h2>{" "}
+      </div>
+      <div className="tebriks">
+        <h1>TEBRİKLER!</h1>
+        <h1>SİPARİŞİNİZ ALINDI</h1>
+      </div>
     </div>
   );
 };
